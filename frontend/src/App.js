9e809346cf84c57c7f49fbc7540194430f2051cf
swapnil chavan <chavanswapnil822@gmail.com>
2023-07-14 15:51:07 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Refill</h1>
+      <Navbar />
     </div>
   );
 }
