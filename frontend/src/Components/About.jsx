12a3525e8e28063cd4c36cb47b90732bcd9df204
swapnil chavan <chavanswapnil822@gmail.com>
2023-07-21 22:11:48 +0530

    update

---
+++ frontend/src/Components/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const OnlinePetrolSellingComponent = () => {
+  return (
+    <div>
+      <h1>Online Petrol Selling</h1>
+      <p>
+        Online petrol selling is a convenient and efficient way to purchase petrol. You can
+        compare prices from different retailers, find the best deal, and have petrol delivered
+        to your door.
+      </p>
+      <ul>
+        <li>Convenient: You can purchase petrol from the comfort of your own home.</li>
+        <li>Efficient: You can compare prices from different retailers and find the best deal.</li>
+        <li>Time-saving: You don't have to go to a petrol station and wait in line.</li>
+        <li>Environmentally friendly: You can reduce your carbon footprint by having petrol delivered.</li>
+      </ul>
+      <p>
+        To learn more about online petrol selling, please visit our website.
+      </p>
+    </div>
+  );
+};
+
+export default OnlinePetrolSellingComponent;
